697341b79c0201f9bc755cf5e3aa8340015e9cb9
Adrian Schuepbach <dri@zurich.ibm.com>
2019-01-30 23:33:30 +0100

    Add activation.

--- mnist/simplemnist.cu
+++ mnist/simplemnist.cu
@@ -45,10 +45,17 @@ static void matrix_add(float *A, int rowA, int colA, float *B, int rowB, int col
     }
 }
 
-static void matrix_scaling(float v, float *A, int rowA, int colA)
+static void matrix_scaling(float v, float *A, int rowA, int colA, float *C)
 {
     for (int i = 0; i < (rowA * colA); i++) {
-        A[i] = v * A[i];
+        C[i] = v * A[i];
+    }
+}
+
+static void matrix_sigma(float *A, int rowA, int colA, float *C)
+{
+    for (int i = 0; i < (rowA * colA); i++) {
+        C[i] = (1.0f / (1.0f + exp(-A[i])));
     }
 }
 
@@ -56,20 +63,26 @@ static void matrix_scaling(float v, float *A, int rowA, int colA)
 static void forward_propagation(float *input,
                                 float *weight1, float *bias1,
                                 float *fc1out, float *fc1biasout,
+                                float *fc1activationout,
                                 float *weight2, float *bias2,
-                                float *fc2out, float *fc2biasout)
+                                float *fc2out, float *fc2biasout,
+                                float *fc2activationout)
 {
     matrix_multiplication(input, 1, 784,
                           weight1, 784, 50,
                           fc1out);
 
     matrix_add(fc1out, 1, 50, bias1, 1, 50, fc1biasout);
+
+    matrix_sigma(fc1biasout, 1, 50, fc1activationout);
     
-    matrix_multiplication(fc1biasout, 1, 50,
+    matrix_multiplication(fc1activationout, 1, 50,
                           weight1, 50, 10,
                           fc2out);
 
     matrix_add(fc2out, 1, 10, bias2, 1, 10, fc2biasout);
+
+    matrix_sigma(fc2biasout, 1, 10, fc2activationout);
 }
 
 
@@ -112,17 +125,21 @@ int create_simple_network(char *trainimg, char *trainlb, char *tstimg, char *tst
     
     float fc1out[50];
     float fc1biasout[50];
+    float fc1activationout[50];
     
     float fc2out[10];
     float fc2biasout[10];
+    float fc2activationout[10];
     
     
     
     forward_propagation(traindesc.databufferf,
                         weights1, bias1,
                         fc1out, fc1biasout,
+                        fc1activationout,
                         weights2, bias2,
-                        fc2out, fc2biasout);
+                        fc2out, fc2biasout,
+                        fc2activationout);
     
     return 0;
 }
